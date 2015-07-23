2990f4703889de9b67cfed08ac8d0ac1260a8754
Julio Lara <juscezari@gmail.com>
2015-07-23 20:07:49 -0300

    Modificado a MainActivity

--- app/src/main/java/app/thender/thender/MainActivity.java
+++ app/src/main/java/app/thender/thender/MainActivity.java
@@ -45,4 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(i);
     }
+
+    public void startLogarConta(View view){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
 }
